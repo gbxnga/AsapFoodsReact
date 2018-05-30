cb26a1342023713041b302ecc3ef1b5b73f01150
gbxnga <iamblizzyy@gmail.com>
2018-05-30 13:53:29 +0100

    added curve to banner

--- components/presentation/KitchenBanner.js
+++ components/presentation/KitchenBanner.js
@@ -1,7 +1,7 @@
 import constants from '../../constants/constants';
 const KitchenBanner = ({kitchen}) =>
-    <div style={{backgroundColor:"white",boxShadow:"0 3px 10px rgba(0, 0, 0, 0.1)",border:"1px solid #e2e6e9", height:275, borderRadius:2,paddingBottom:15,margin:"15px 0"}}>
-        <img style={{width:"100%"}} height="180" src={`${constants.site}/src/images/${kitchen.description}-in.jpeg`} />
+    <div style={{backgroundColor:"white",boxShadow:"0 3px 10px rgba(0, 0, 0, 0.1)",border:"1px solid #e2e6e9", height:275, borderRadius:10,paddingBottom:15,margin:"15px 0"}}>
+        <img style={{width:"100%",borderTopLeftRadius:10,borderTopRightRadius:10}} height="180" src={`${constants.site}/src/images/${kitchen.description}-in.jpeg`} />
         <div className="col-xs-12">
             <h3 style={{marginTop:15}}>{kitchen.kitchen}</h3>
             <address style={{fontSize:14}}>{kitchen.address}</address>
