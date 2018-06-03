417718f7534d39c05ba21af14d8b8a280654e227
gbxnga <iamblizzyy@gmail.com>
2018-06-03 14:09:46 +0100

    added phone number verification functionality

--- components/container/RegisterContainer.js
+++ components/container/RegisterContainer.js
@@ -1,29 +1,242 @@
-import React, {Component} from 'react'
+import React, {
+    Component
+} from 'react'
 import Register from '../presentation/Register'
-import { connect } from 'react-redux'
+import {
+    connect
+} from 'react-redux'
 import user from '../../reducers/user'
 import registerUser from '../../actions/registerUser'
-/*class RegisterContainer extends Component
-{
-    constructor(props)
-    {
+import C from '../../constants/constants';
+import toast from '../../modules/toast'
+import {
+    Link
+} from 'react-router-dom'
+import axios from "axios";
+class RegisterContainer extends Component {
+    constructor(props) {
         super(props)
+        this.state = {
+            name: '',
+            password: '',
+            email: '',
+            phone: '',
+
+            isValidatingPhoneNumber: false,
+            validationCode: '',
+            warning: '',
+            loading: false
+
+        }
+        this._registerUser = this._registerUser.bind(this)
+        this._validateParameters = this._validateParameters.bind(this)
+        this._sendValidationCode = this._sendValidationCode.bind(this)
+        this._validateSentCode = this._validateSentCode.bind(this)
+
+
+    }
+    _sendValidationCode(phone, name) {
+
+        let formData = new FormData();
+
+        formData.append("phone", phone);
+        formData.append("name", name);
+
+        axios.post(C.VALIDATE_PHONE_NUMBER_API, formData)
+            .then(response => {
+                console.log(response)
+                return response
+            })
+            .then(json => {
+                if (json.data.success) {
+                    this.setState({
+                        isValidatingPhoneNumber: true
+                    })
+                }
+                else{
+                    toast(`Couldn't send code to ${phone}`)
+                }
+            })
+            .catch((error) => {
+                //return false
+                toast('An Error Occured! Please try again')
+                console.log(`${formData} ${error}`)
+            });
     }
 
-    componentWillMount()
-    {
+    _validateSentCode(e) {
 
+        e.preventDefault()
+
+        const {name, password, email, phone, validationCode} = this.state
+
+        let formData = new FormData();
+
+        formData.append("code", validationCode);
+        formData.append("phone", phone);
+
+        axios.post(C.VALIDATE_SENT_CODE_API, formData)
+            .then(response => {
+                console.log(response)
+                return response
+            })
+            .then(json => {
+                if (json.data.success) {
+                    // register the user
+                    this.props.registerUser(0,name, password,name, email, phone,"","email")
+                }
+                else{
+                    toast(`Couldn't validate code to ${phone}`)
+                }
+            })
+            .catch((error) => {
+                //return false
+                toast('An Error Occured! Please try again')
+                console.log(`${formData} ${error}`)
+            });
+
+
+
+    }
+    _registerUser(e) {
+        e.preventDefault()
+        const {
+            name,
+            password,
+            email,
+            phone
+        } = this.state
+
+        if (!this._validateParameters(name, password, email, phone)) return;
+
+        //this.setState({loading:true})
+        this._sendValidationCode(phone, name)
+
+
+        //registerUser(0,name, password,name, email, phone,"","email")
+
+    }
+
+    _validateParameters(name, password, email, phone) {
+
+        var regexPhone = /^\d{11}$/;
+        var regexName = /^[a-zA-Z0-9 \.]{3,100}$/;
+        var regexAddress = /^[a-zA-Z0-9 \.]{3,100}$/;
+        var regexUsername = /^[a-zA-Z0-9]{5,12}$/;
+        var regexPassword = /^[a-zA-Z_0-9]{5,12}$/;
+        var regexEmail = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@{[a-zA-Z0-9_\-\.]+0\.([a-zA-Z]{2,5}){1,25})+)*$/;
+
+
+        if (!regexPhone.test(phone)) {
+            this.setState({
+                warning: 'Phone number must be 11 digits'
+            });
+            toast("Phone number invalid!");
+            return false;
+        } else if (!regexName.test(name)) {
+            this.setState({
+                warning: 'Username must be 5-12 characters and(or) digits long'
+            });
+            toast("Name invalid!");
+            return false;
+        } else if (!regexEmail.test(email)) {
+            this.setState({
+                warning: 'Email should be a standard, acceptable email'
+            });
+            toast("Email invalid!");
+            return false;
+        } else if (!regexPassword.test(password.value)) {
+            this.setState({
+                warning: 'Password must be 5-12 characters and(or) digits long'
+            });
+            toast("Password invalid!");
+            return false;
+        } else
+            return true
+    }
+    _handleChange(inputName, e) {
+        console.log(inputName, e.target.value);
+
+        switch (inputName) {
+            case 'name':
+                this.setState({
+                    name: e.target.value
+                })
+                break;
+            case 'phone':
+                this.setState({
+                    phone: e.target.value
+                })
+                break;
+            case 'password':
+                this.setState({
+                    password: e.target.value
+                })
+                break;
+            case 'email':
+                this.setState({
+                    email: e.target.value
+                })
+                break;
+            case 'validationCode':
+                this.setState({
+                    validationCode: e.target.value
+                })
+                break;
+
+        }
     }
 
 
     render(){
-        return(
+        /*return(
+
                 <Register registerUser={registerUser}/>
             
+        )*/
+        const {name, password, phone, email, isValidatingPhoneNumber, validationCode} = this.state
+        console.log(JSON.stringify(this.state))
+        return(
+            (isValidatingPhoneNumber) ?
+
+            <div className="register-page page-container" id=" " className="col-md-12" style={{marginTop:"10%", display:"block"}}>
+    
+            <div id="landing-page-banner">
+                <img width="150 " height="150 " className="center-block " src="src/icons/logo 192.png " />
+            </div>
+    
+            <form id="register-form" onSubmit={e => this._validateSentCode(e)} action=" " method="post " style={{marginTop:"-35px"}}>
+                <p style={{display:"none", width:"90%"}} className="alert alert-warning center-block text-center warning"></p>
+                <p style={{display:"none", width:"90%"}} className="alert alert-info center-block text-center warning">Enter the 4 digit code sent to <strong>{phone}</strong></p>
+                <input onChange={this._handleChange.bind(this, 'validationCode')} style={{backgroundColor:"white",border:"1px solid #cccccc"}} defaultValue={validationCode} autoComplete="off" id="username-input" name="code" type="text" className="center-block" placeholder="Validation Code " />
+                <button type="submit" className="landing-page-btn center-block text-center" id="email-login-btn" style={{width:"80%",border:"none",height:44,boxShadow: "0px 8px 15px rgba(0, 0, 0, 0.1)"}} href="#facebook ">Validate Number</button>
+            </form>
+            <p><Link id="dont-have-account-btn " style={{color:"#333333"}} className="text-center center-block" to="login"><span style={{fontSize:13,marginRight:5}} className="glyphicon glyphicon-arrow-left"></span>Back to homepage</Link></p>
+    
+            </div>
+            :
+            <div className="register-page page-container" id=" " className="col-md-12" style={{marginTop:"10%", display:"block"}}>
+    
+            <div id="landing-page-banner">
+                <img width="150 " height="150 " className="center-block " src="src/icons/logo 192.png " />
+            </div>
+    
+            <form id="register-form" onSubmit={e => this._registerUser(e)} action=" " method="post " style={{marginTop:"-35px"}}>
+                <p style={{display:"none", width:"90%"}} className="alert alert-warning center-block text-center warning"></p>
+                <input onChange={this._handleChange.bind(this, 'name')} style={{backgroundColor:"white",border:"1px solid #cccccc"}} defaultValue={name} autoComplete="off" id="username-input" name="username" type="text" className="center-block" placeholder="Fullname " />
+                <input onChange={this._handleChange.bind(this, 'phone')}  defaultValue={phone} style={{backgroundColor:"white",border:"1px solid #cccccc"}} autoComplete="off" id="phone-input" name="phone" type="number" className="center-block" placeholder="Phone number " />
+                <input onChange={this._handleChange.bind(this, 'email')} defaultValue={email} style={{backgroundColor:"white",border:"1px solid #cccccc"}} autoComplete="off" id="email-input-2" name="email" type="email" className="center-block" placeholder="email " />
+    
+                <input onChange={this._handleChange.bind(this, 'password')} defaultValue={password} style={{backgroundColor:"white",border:"1px solid #cccccc"}}  autoComplete="off" id="password-input-2" name="password" type="password" className="center-block" placeholder="password" />
+                <button type="submit" className="landing-page-btn center-block text-center" id="email-login-btn" style={{width:"80%",border:"none",height:44,boxShadow: "0px 8px 15px rgba(0, 0, 0, 0.1)"}} href="#facebook ">Register</button>
+            </form>
+            <p><Link id="dont-have-account-btn " style={{color:"#333333"}} className="text-center center-block" to="login"><span style={{fontSize:13,marginRight:5}} className="glyphicon glyphicon-arrow-left"></span>Back to homepage</Link></p>
+    
+            </div>
         )
     }
-}*/
-const RegisterContainer =  connect(
+}
+export default connect(
     (state, props) => { 
         return {
             user : user
@@ -35,5 +248,5 @@ const RegisterContainer =  connect(
                 registerUser(id,username, password,name, email, phone, address,type,dispatch)
             }
         })
-)(Register)
-module.exports = RegisterContainer;
+)(RegisterContainer)
+//module.exports = RegisterContainer;
